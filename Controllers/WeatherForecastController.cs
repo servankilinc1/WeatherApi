b54421848e5362028ef5bdba36fd33c5d84584d7
servan kılınç <121825907+servankilinc1@users.noreply.github.com>
2023-11-18 22:56:25 +0300

    second commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -11,6 +11,9 @@ namespace WeatherApi.Controllers
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+
+
+
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
